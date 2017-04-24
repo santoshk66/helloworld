b76e0cbd0afa36480e00f991a9da98379c6170da
santoshk66 <santosh.k66@gmail.com>
2017-04-24 18:07:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ font-size=70;
 	<h1> ok man we have done it </h1>
 	
 	<h2> This is the new stuff</h2>
-	<h2 style="text-align:center; font-size=20; color=20;"> adding a new code </h2>
+	<h2 style="text-align:center; font-size=20; color=20;"> adding a new code done </h2>
 
 </body>
 </html>
