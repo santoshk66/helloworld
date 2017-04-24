c5d89e8cb04608788022279e37d49c1c094b1013
Santosh Kumar <santosh4.kumar@one97.delhi.net>
2017-04-24 17:58:03 +0530

    ngofix" quit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<style>
+h1 {
+font-size=70;	
+	
+} 
+</style>
+	<title> welcome world </title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1> ok man we have done it </h1>
+	
+	<h2> This is the new stuff</h2>
+	<h2 style="text-align:center; font-size=20; color=20;"> adding a new code </h2>
+
+</body>
+</html>
